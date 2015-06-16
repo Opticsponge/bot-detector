dd8c7ae0ee50dd3d9ffcfdd15e39779c99786382
Shawn Looker <shawn.looker@gmail.com>
2015-06-16 12:08:23 -0700

    Updated a few things so that it actually works, added some feedback on when the UA's are loaded.

--- botDetect.js
+++ botDetect.js
@@ -1,43 +1,46 @@
+'use strict';
 var request = require('request');
 var parseString = require('xml2js').parseString;
 
 var url = 'http://www.user-agents.org/allagents.xml';
 
 var ua_list = {};
+var exports = module.exports = {};
 
-module.exports = {
-  updateXML: function(callback) {
-    request(url, function (error, response, xml) {
-      if (!error && response.statusCode == 200) {
-        parseString(xml, function(err, result) {
-          var list = JSON.stringify(result);
-          var uas = result['user-agents']['user-agent'];
-          uas.forEach(function(ua) {
-            ua_list[ua['String'][0]] = 1;
-            ua_list.push({
-              agent_string: ua['String'][0],
-              description: ua['Description'][0]
-            });
-          });
-          callback();
+var updateXML = function(callback) {
+  request(url, function (error, response, xml) {
+    if (!error && response.statusCode == 200) {
+      parseString(xml, function(err, result) {
+        var uas = result['user-agents']['user-agent'];
+        uas.forEach(function(ua) {
+          ua_list[ua.String[0]] = 1;
         });
-      }
-    });
-  },
-  printList: function() {
-    console.log(ua_list);
-  },
-
-  uaCount: function(callback) {
-    callback(Object.keys(ua_list).length);
-  },
-
-  isBot: function(ua_string) {
-    if (ua_list[ua_string]) {
-      return true;
+        console.log("User-Agent list loaded");
+        if (callback) {
+          callback();
+        }
+      });
     }
-    return false;
-  }
+  });
 }
+exports.updateXML = updateXML;
+
+var printList = function() {
+  console.log(ua_list);
+}
+exports.printList = printList;
+
+var uaCount = function(callback) {
+  callback(Object.keys(ua_list).length);
+}
+exports.uaCount = uaCount;
+
+var isBot = function(ua_string) {
+  if (ua_list[ua_string]) {
+    return true;
+  }
+  return false;
+}
+exports.isBot = isBot;
 
 updateXML();
