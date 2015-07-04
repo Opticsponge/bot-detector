30e366a311bb3804b16aff82629996ed0b1c2a77
Shawn Looker <shawn.looker@gmail.com>
2015-07-04 14:47:01 -0700

    Added in custom bot list support.

--- botDetect.js
+++ botDetect.js
@@ -1,31 +1,8 @@
 'use strict';
-var request = require('request');
-var parseString = require('xml2js').parseString;
 var fs = require('fs');
-
-var url = 'http://www.user-agents.org/allagents.xml';
 
 var ua_list = {};
 var exports = module.exports = {};
-
-// This can be called manually and piped into botlist.json to update the botlist
-var updateXML = function(callback) {
-  request(url, function (error, response, xml) {
-    if (!error && response.statusCode == 200) {
-      parseString(xml, function(err, result) {
-        var uas = result['user-agents']['user-agent'];
-        uas.forEach(function(ua) {
-          ua_list[ua.String[0]] = 1;
-        });
-        console.log(JSON.stringify(ua_list));
-        if (callback) {
-          callback();
-        }
-      });
-    }
-  });
-}
-exports.updateXML = updateXML;
 
 var printList = function() {
   console.log(ua_list);
@@ -46,14 +23,28 @@ var isBot = function(ua_string) {
 exports.isBot = isBot;
 
 var loadBotList = function(callback) {
+  // Read bot list from user-agents.org
   fs.readFile('botlist.json', 'utf-8',  function(err, data) {
     if (err) {
       callback(err); 
     }
     ua_list = JSON.parse(data);
-    callback(null);
+
+    // Read custom botlist
+    fs.readFile('customBotList.json', 'utf-8', function(err, data) {
+      if (err) {
+        callback(err); 
+      }
+      var customs = JSON.parse(data);
+      for (var attrname in customs) { 
+        ua_list[attrname] = customs[attrname]; 
+      }
+      callback(null);
+    });
   });
 }
+// Added so that tests would wait until the bot list is loaded before executing
+exports.loadBotList = loadBotList;
 
 loadBotList(function() {
    uaCount(function(count) {
--- test/test.js
+++ test/test.js
@@ -2,20 +2,11 @@ var should = require('chai').should(),
     botDetect = require('../botDetect');
 
 before(function(done) {
-	// remove database data here
-	botDetect.updateXML(function() {
-		done();
-	});
-}) 
-
-describe('updateXML', function() {
-  it('fetches the xml file', function(done) {
-  	botDetect.uaCount(function(count) {
-  	 	count.should.be.above(2000);
-  	 	done();
-  	});
+  // remove database data here
+  botDetect.loadBotList(function() {
+    done();
   });
-});
+}) 
 
 describe('botDetection', function() {
 	it('test a positive detection', function() {
@@ -26,6 +17,10 @@ describe('botDetection', function() {
 	it ('tests a negative detection', function() {
 		botDetect.isBot('Mozilla/5.0 (Linux; <Android Version>; <Build Tag etc.>) AppleWebKit/<WebKit Rev> (KHTML, like Gecko) Chrome/<Chrome Rev> Mobile Safari/<WebKit Rev>')
 			.should.be.false;
-
 	});
+
+  it('tests a positive from custom bot list', function() {
+		botDetect.isBot('bitlybot')
+			.should.be.true;
+  });
 });
